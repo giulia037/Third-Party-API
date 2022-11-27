e5327af6b7e19d6d7a8d0b6429a59336d05ae64d
giulia shelton <giuliashelton6875@gmail.com>
2022-11-27 18:36:50 -0500

    worked with a tutor to fix issues in code found the bug in java and site is running properly now

--- README.md
+++ README.md
@@ -6,9 +6,7 @@ For this challenge we were supposed to create a website where we would be able t
 
 # Screen Shot
 
-![image](https://user-images.githubusercontent.com/114687261/199849901-03001bc8-dfde-4270-8ee8-e0572bb5a307.png)
 
 
 # Deployed Site
 
-https://giulia037.github.io/Third-Party-API/
--- script.js
+++ script.js
@@ -1,101 +1,52 @@
-// Moment.js
-var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
-var currentHour = moment().format('h:mm:ss a');
-// Text hour var
-var nineAm = $("#9am");
-var tenAm = $("#10am");
-var elevenAm = $("#11am");
-var twelvePm = $("#12pm");
-var onePm = $("#13pm");
-var twoPm = $("#14pm");
-var threePm = $("#15pm");
-var fourPm = $("#16pm");
-var fivePm = $("#17pm");
-var sixPm = $("#18pm");
-var sevenPm = $("#19pm");
-
-var hour = moment().hours();
-var userInput;
-var hourSpan;
-// var hourString = $(".hour").text().split(" ");
-
-// Date and Hour
-
-var interval = setInterval(function() {
-  var momentNow = moment();
-  $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
-                      + momentNow.format('dddd')
-                       .substring(0,3).toUpperCase());
-  $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
-}, 100);
-
-function initPage() {
-
-  console.log("Current Hour " + hour);
-  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
-  nineAm.val(init9);
-
-  var init10 = JSON.parse(localStorage.getItem("10:00 am"))
-  tenAm.val(init10);
-  
-  var init11 = JSON.parse(localStorage.getItem("11:00 am"))
-  elevenAm.val(init11);
-  
-  var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
-  twelvePm.val(init12);
-  
-  var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
-  onePm.val(init1);
-  
-  var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
-  twoPm.val(init2);
-  
-  var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
-  threePm.val(init3);
- 
-  var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
-  fourPm.val(init4);
-  
-  var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
-  fivePm.val(init5);
-  
-  var init6 = JSON.parse(localStorage.getItem("06:00 pm"))
-  sixPm.val(init6);
-  
-  var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
-  sevenPm.val(init7);
-} 
-
-function background () {
-      
-  $(".form-control").each(function () {
-      var timeTest = parseInt($(this).attr("id"));
-      hour = parseInt(hour);
-      console.log(timeTest);
-      console.log(hour);
-//      console.log(this);
-      if (hour > timeTest) {
-          $(this).addClass("past");
-      } else if (hour < timeTest) {
-          $(this).addClass("future");
-      } else {
-          $(this).addClass("present");
-      }
-  });
-}
-
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
 $(document).ready(function(){
-  initPage()
-  background()
+    var timeDisplayEl = $('#currentDay.lead');
+    
+    function displayTime() {
+        var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+        timeDisplayEl.text(rightNow);
+      }
+      
+      displayTime();
+    
+      $('.saveBtn').on ('click', function(){
+        const value = $(this).siblings('.description').val()
+        const time = $(this).parent().attr('id')
+        localStorage.setItem(time,value)
+    }) 
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'))
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'))
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'))
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'))
+    $('#hour-1 .description').val(localStorage.getItem('hour-1'))
+    $('#hour-2 .description').val(localStorage.getItem('hour-2'))
+    $('#hour-3 .description').val(localStorage.getItem('hour-3'))
+    $('#hour-4 .description').val(localStorage.getItem('hour-4'))
+    $('#hour-5 .description').val(localStorage.getItem('hour-5'))
+    
+    function workDay() {
+        const currentHour = dayjs().hour()
+        console.log(currentHour)
+        $('.time-block').each(function() {
+            const hour = parseInt($(this).attr('id').split('-')[1])
+            console.log(hour)
+            if(currentHour > hour){
+                $(this).removeClass('present')
+                $(this).removeClass('future')
+                $(this).addClass('past')
+             } else if(currentHour === hour){
+                $(this).removeClass('past')
+                $(this).removeClass('future')
+                $(this).addClass('present')
+             }else {
+                $(this).addClass('future')
+             }
+        })
+        
+    }
 
-  // Buttons (save to Local Storage)
-  $(".saveBtn").on("click", function(){
-    userInput = $(this).siblings(".form-control").val().trim();
-    console.log(userInput);
-    hourSpan = $(this).siblings(".input-group-prepend").text().trim();
-    console.log(hourSpan);
-    localStorage.setItem(hourSpan, JSON.stringify(userInput));
+workDay()
+})
 
-  })
-
-});
